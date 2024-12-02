9f32b06cf791e8f8458e7c02e6a42416b6e6de26
Saint21082007 <36681@bpk.ac.th>
2024-12-02 14:44:47 +0700

    Add files via upload

---
+++ non-4.py
@@ -0,0 +1,24 @@
+province = [
+    "กรุงเทพมหานคร", "กาฬสินธุ์", "กาญจนบุรี", "จันทบุรี", 
+    "ฉะเชิงเทรา", "ชลบุรี", "มะเยา", "ชุมพร", 
+    "ชัยนาท", "ขอนแก่น", "ชัยภูมิ", "ยโสธร", 
+    "นครพนม", "นครราชสีมา", "เชียงใหม่"
+]
+
+
+province[province.index("กรุงเทพมหานคร")] = "Bangkok"
+
+
+province[province.index("นครราชสีมา")] = "Korat"
+
+
+province[province.index("ฉะเชิงเทรา")] = "Paet Riw"
+
+
+province[province.index("ชลบุรี")] = "Chon buri"
+
+
+province[province.index("ชุมพร")] = "Chum porn"
+
+
+print(province)
